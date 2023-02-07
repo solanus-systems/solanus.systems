e165d5bf40c345c48977c662384c5121a0e1bb51
Nick Budak <budak@stanford.edu>
2023-02-07 13:05:58 -0800

    Set up multi-page compilation in Vite

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,28 @@
-const path = require('path')
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
-export default {
-  root: path.resolve(__dirname, 'src'),
-  build: {
-    outDir: path.resolve(__dirname, 'dist'),
-    emptyOutDir: true,
+const root = resolve(__dirname, 'src')
+
+export default defineConfig({
+  root: root,
+  resolve: {
+    alias: {
+      '~bootstrap': resolve(__dirname, 'node_modules/bootstrap'),
+    }
   },
-  publicDir: path.resolve(__dirname, 'public'),
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        about: resolve(root, 'about/index.html'),
+      }
+    }
+  },
+  publicDir: resolve(__dirname, 'public'),
   server: {
     port: 8080,
     hot: true
   },
-}
+})

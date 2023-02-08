c54f8928e0f10e18532862d83f7d375a4dee2e73
Nick Budak <budak@stanford.edu>
2023-02-08 13:51:21 -0800

    Add static forms middleware

---
+++ functions/_middleware.ts
@@ -0,0 +1,11 @@
+import staticFormsPlugin from "@cloudflare/pages-plugin-static-forms"
+
+// Intercept form submissions for the waitlist and store them in KV
+export const onRequest: PagesFunction = staticFormsPlugin({
+  respondWith: ({ formData }) => {
+    const name = formData.get("name")
+    const email = formData.get("email")
+
+    return new Response(`Hello ${name}! We'll email you at ${email}.`)
+  }
+})

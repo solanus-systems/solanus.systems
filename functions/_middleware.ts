87390c79c6d8e9722422024dd73cca38b7e7149f
Nick Budak <budak@stanford.edu>
2023-02-09 21:38:12 -0800

    Implement waitlist signups using mailchannels function
    Closes #3

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,11 +1,41 @@
-import staticFormsPlugin from "@cloudflare/pages-plugin-static-forms"
+import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels"
 
-// Intercept form submissions for the waitlist and store them in KV
-export const onRequest: PagesFunction = staticFormsPlugin({
-  respondWith: ({ formData }) => {
-    const name = formData.get("name")
-    const email = formData.get("email")
+// @ts-ignore
+import template from "./waitlist-email.html"
 
-    return new Response(`Hello ${name}! We'll email you at ${email}.`)
-  },
-})
+// Environment variables set in the Pages dashboard
+interface Env {
+  DKIM_PRIVATE_KEY: string
+}
+
+// Send an email to the address provided in the waitlist form
+export const onRequest: PagesFunction<Env> = (context) =>
+  mailChannelsPlugin({
+    personalizations: ({ formData }) => {
+      return [
+        {
+          to: [{ email: formData.get("email"), name: formData.get("name") }],
+          dkim_domain: "solanus.systems",
+          dkim_selector: "mailchannels",
+          dkim_private_key: context.env.DKIM_PRIVATE_KEY,
+        },
+      ]
+    },
+    from: {
+      email: "info@solanus.systems",
+      name: "Solanus",
+    },
+    subject: "Thanks for your interest in Solanus",
+    content: () => [
+      {
+        type: "text/html",
+        value: template,
+      },
+    ],
+    respondWith: ({ request, formData }) => {
+      if (request.method !== "POST")
+        return new Response("Method not allowed", { status: 405 })
+
+      return new Response(`Email sent to ${formData.get("email")}`)
+    },
+  })(context)

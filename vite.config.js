3cea8ac05d9216cb623fb4f234e0b9967f570148
Nick Budak <budak@stanford.edu>
2023-02-22 13:37:29 -0800

    Create an about page
    Closes #5

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(root, "index.html"),
+        about: resolve(root, "about.html"),
         404: resolve(root, "404.html"),
       },
     },
